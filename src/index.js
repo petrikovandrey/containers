83c7e512cfc98cec17ce11b8db464feac93ddfff
Andrey Petrikov <aapetrikov@gmail.com>
2020-07-07 19:57:56 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -15,3 +15,16 @@ export class Team {
     }
 }
 
+export class ErrorRepository {
+    constructor(code, description) {
+        this.error = new Map;
+        this.error.set(code, description)
+    }
+    translate(code) {
+        if (this.error.has(code)) {
+            return this.error.get(code);
+        } else {
+            return "Unknown error";
+        }
+    }
+}
