45ea561f8f496ca5fc6bf76c3f97082d1156f1c0
petrikovandrey <aapetrikov@gmail.com>
2021-07-12 18:07:15 +0300

    eslint

--- src/test.js
+++ src/test.js
@@ -1,16 +1,16 @@
-import * as index from "../src/index.js";
+import * as index from './index';
 
 
-test("new error", () => {
-    const result = new index.ErrorRepository("404", "not found");
-    const expected = new Map;
-    expected.set("404", "not found");
-    expect(result.error).toEqual(expected);
-})
+test('new error', () => {
+  const result = new index.ErrorRepository('404', 'not found');
+  const expected = new Map();
+  expected.set('404', 'not found');
+  expect(result.error).toEqual(expected);
+});
 
-test("translate", () => {
-    const error = new index.ErrorRepository("404", "not found");
-    const result = error.translate("500");
-    const expected = "Unknown error";
-    expect(result).toEqual(expected);
-})
+test('translate', () => {
+  const error = new index.ErrorRepository('404', 'not found');
+  const result = error.translate('500');
+  const expected = 'Unknown error';
+  expect(result).toEqual(expected);
+});
